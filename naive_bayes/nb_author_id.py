1d7e24d06bc4b1c5274bb8e06110d06005926fe6
ISA-TUNCMAN <isatuncman@gmail.com>
2017-10-05 13:17:24 -0700

    Solve Naive Bayes Classification Exercise

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
@@ -25,9 +26,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-t1, 3), "s"
+
+counter = 0
+for i in range(len(pred)):
+    if labels_test[i] == pred[i]:
+        counter += 1.0
+
+print counter/len(pred)
+#print clf.score(features_test, labels_test)
+
 
 
 #########################################################
-
-
