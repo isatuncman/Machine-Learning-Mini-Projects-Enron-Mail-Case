c6af6dfe3bf7103caa05f9b6315b4d4fef54f453
ISA-TUNCMAN <isatuncman@gmail.com>
2017-10-10 11:57:14 -0700

    Feat: Remove outliers of mini project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,18 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    errors = predictions - net_worths
+
+    mydata = []
+    for i in range(len(errors)):
+        mytuple = ages[i], net_worths[i], errors[i]
+        mydata.append(mytuple)
+
+
+    cleaned_data = sorted(mydata, key = lambda x: x[2])[:-9]
 
     ### your code goes here
-
-    
     return cleaned_data
-
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,7 +26,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+#print reg.coef_
+#print reg.score(ages_train, net_worths_train)
 
+#print reg.score(ages_test, net_worths_test)
 
 
 
@@ -68,11 +74,14 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print reg.coef_
+        print reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
         print "   either way, only draw the scatter plot of the cleaned data"
+
     plt.scatter(ages, net_worths)
     plt.xlabel("ages")
     plt.ylabel("net worths")
@@ -81,4 +90,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
